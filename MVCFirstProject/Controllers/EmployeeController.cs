b1950d756e6db1a0bcfe1948a790144b2cb73f09
srinivasbuddana <anusri9701@gmail.com>
2022-07-20 07:37:45 +0530

    git-MVC project
    Employee

---
+++ MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstProject.Models;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Employeeinfo()
+        {
+
+            Employee employee = new Employee()
+            {
+                Employeeld = 1001,
+                EmployeeName = "Khaja Moiz",
+                EmployeeLocation = "Hyderabad"
+            };
+
+            return View(employee);
+        }
+    }
+}
---
+++ MVCFirstProject/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFirstProject.Models
+{
+    public class Employee
+    {
+        public int Employeeld { get; set; }
+        public string EmployeeName { get;set; }
+        public string EmployeeLocation { get; set; }
+
+    }
+}
